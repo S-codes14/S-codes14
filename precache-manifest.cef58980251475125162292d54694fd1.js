b3a679c38c06e700d506877293bfde5e77bd1d32
S-codes14 <smlmnguni14@gmail.com>
2021-06-12 14:47:24 +0200

    Updates

---
+++ precache-manifest.cef58980251475125162292d54694fd1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6262afd03c0cdc5c8e52b31198ad283a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4784c9a7e7a2a5fa17f",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "0e62c97f89528a21b89e",
+    "url": "/static/css/4.e568d4fa.chunk.css"
+  },
+  {
+    "revision": "51c10b9c111a7f8f5917",
+    "url": "/static/css/main.239750f5.chunk.css"
+  },
+  {
+    "revision": "30f8e237b67c511c4a0c",
+    "url": "/static/js/2.41a78429.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.41a78429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4784c9a7e7a2a5fa17f",
+    "url": "/static/js/3.e35ed70c.chunk.js"
+  },
+  {
+    "revision": "0e62c97f89528a21b89e",
+    "url": "/static/js/4.68ab67e6.chunk.js"
+  },
+  {
+    "revision": "51c10b9c111a7f8f5917",
+    "url": "/static/js/main.fb07f17c.chunk.js"
+  },
+  {
+    "revision": "a2646b84055c150af8da",
+    "url": "/static/js/runtime-main.8aa672b4.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "d94873bd212de1587493647eb2594f87",
+    "url": "/static/media/ph.d94873bd.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "b3d56ecb5eeb781380632817ac0f6a1e",
+    "url": "/static/media/rando.b3d56ecb.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "950ac1dc115f1f6cb66327f21cb4118c",
+    "url": "/static/media/talksCardBack.950ac1dc.svg"
+  }
+]);
